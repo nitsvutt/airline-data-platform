5bd8ff3c96cbf6401d2e8043cef7d9a59e8bdd01
nitsvutt <nitsvutt@gmail.com>
2023-06-08 15:03:28 +0700

    3 partition config

--- kafka/config.sh
+++ kafka/config.sh
@@ -2,9 +2,9 @@
 
 docker exec -it connect bash
 
-(echo 1; sleep 20; echo y; sleep 20; echo y; sleep 20; echo y) | /bin/confluent-hub install confluentinc/kafka-connect-jdbc:10.7.1
-(echo 1; sleep 20; echo y; sleep 20; echo y; sleep 20; echo y) | /bin/confluent-hub install mongodb/kafka-connect-mongodb:1.10.0
-(echo 1; sleep 20; echo y; sleep 20; echo y; sleep 20; echo y) | /bin/confluent-hub install confluentinc/kafka-connect-avro-converter:7.4.0
+/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-jdbc:10.7.1
+/bin/confluent-hub install --no-prompt mongodb/kafka-connect-mongodb:1.10.0
+/bin/confluent-hub install --no-prompt confluentinc/kafka-connect-avro-converter:7.4.0
 
 cd /usr/share/confluent-hub-components/confluentinc-kafka-connect-jdbc/
 curl -O https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.30/mysql-connector-java-8.0.30.jar

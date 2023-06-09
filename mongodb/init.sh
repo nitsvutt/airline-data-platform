3b6539398dd8b4277835bf00b34b549977eb2781
nitsvutt <nitsvutt@gmail.com>
2023-06-09 22:30:27 +0700

    define services and config network

---
+++ mongodb/init.sh
@@ -0,0 +1,8 @@
+rs.initiate({
+  _id: "rs0",
+  members: [
+    { _id: 0, host: "mongo-1:27017" },
+    { _id: 1, host: "mongo-2:27017" },
+    { _id: 2, host: "mongo-3:27017" }
+  ]
+});
--- mongodb/mongodb.sh
+++ mongodb/mongodb.sh
@@ -1,6 +1,12 @@
+#!/bin/sh
+
+docker network create -d bridge mongodb
+
 docker run --name mongodb -d \
     -p 27017:27017 \
     -e MONGO_INITDB_ROOT_USERNAME=root \
     -e MONGO_INITDB_ROOT_PASSWORD=admin \
     -v /Users/trantrieuvu/Desktop/mongodb:/data/db \
     mongo:latest
+
+docker exec -it mongo-1 mongosh | cat init.sh

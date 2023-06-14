a951d20777946c180b48d2ef70cf0fe900efba69
nitsvutt <nitsvutt@gmail.com>
2023-06-14 11:14:51 +0700

    insert data to source collection

--- kafka/script.sql
+++ kafka/script.sql
@@ -1,3 +1,4 @@
+---MYSQL
 -- grant privileges for the user
 grant all privileges on flight_delay.* to 'kafka';
 
@@ -6,3 +7,7 @@ alter table flight records modify EntryDate datetime;
 
 -- create a test table
 create table source as select * from flight_records limit 1000000;
+
+---MONGODB
+--create a test table
+db.source.insertMany(db.flight_records.find().limit(100000).toArray());

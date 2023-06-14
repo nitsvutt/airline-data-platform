e2f7023e0c01dc682c3a9a3b7748932de4855888
nitsvutt <nitsvutt@gmail.com>
2023-06-14 10:30:16 +0700

    3 brokers set up

--- mongodb/mongodb.sh
+++ mongodb/mongodb.sh
@@ -10,8 +10,8 @@ docker run --name mongodb -d \
     -p 27017:27017 \
     -e MONGO_INITDB_ROOT_USERNAME=root \
     -e MONGO_INITDB_ROOT_PASSWORD=admin \
-    -v /Users/trantrieuvu/Desktop/mongodb:/data/db \
+    -v /Users/trantrieuvu/Desktop/mongodb1:/data/db \
     mongo:latest
 
 docker exec -it mongo-1 mongosh -u root -p admin | cat init.sh
-# note that you should exit the current session and log in again before interacting
+# note that you should exit the current session and log in again after the restart process finished
--- mysql/script.sql
+++ mysql/script.sql
@@ -2,4 +2,7 @@
 grant all privileges on flight_delay.* to 'kafka';
 
 -- change datatype of 'EntryDate' column to datetime
-alter table flight reocrds modify EntryDate datetime;
+alter table flight records modify EntryDate datetime;
+
+-- create a test table
+create table source as select * from flight_records limit 1000000;

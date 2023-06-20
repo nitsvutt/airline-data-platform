1090193ff13482d11675504ca09de560af011079
nitsvutt <nitsvutt@gmail.com>
2023-06-20 09:52:44 +0700

    increase cassandra timeout

--- setup-cassandra/cassandra.sh
+++ setup-cassandra/cassandra.sh
@@ -6,7 +6,9 @@ docker run --name cassandra -d \
     -p 9042:9042 \
     -v /Users/trantrieuvu/Desktop/cassandra:/var/lib/cassandra \
     --network cassandra \
-    --restart always\
+    --restart always \
     cassandra:latest
 
-docker exec -it cassandra cqlsh
+docker cp /Users/trantrieuvu/Documents/Workspace/airline-data-platform/setup-cassandra/cassandra.yaml cassandra:/etc/cassandra/
+
+docker exec -it cassandra cqlsh --request-timeout=6000

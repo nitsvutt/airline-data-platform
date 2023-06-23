c39f98b6a24826834c980309ce7b50e756380e98
nitsvutt <nitsvutt@gmail.com>
2023-06-23 20:38:59 +0700

    update set up mongodb

--- setup-source/mongodb/mongodb.sh
+++ setup-source/mongodb/mongodb.sh
@@ -11,13 +11,10 @@ mkdir -p ~/Desktop/mongos/db1
 mkdir -p ~/Desktop/mongos/db2
 mkdir -p ~/Desktop/mongos/db3
 
-# set up 3 replicas
+# start replicaset
 mongod --port 2717 --dbpath ~/Desktop/mongos/db1 --replSet myReplicaSet
 mongod --port 2727 --dbpath ~/Desktop/mongos/db2 --replSet myReplicaSet
 mongod --port 2737 --dbpath ~/Desktop/mongos/db3 --replSet myReplicaSet
-
-# connect to one replica
-mongosh --port 2717
 
 # stop replicaset
 mongosh --port 2737 --eval 'db.adminCommand("shutdown");'

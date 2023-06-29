bb614ff188c3e5d22c4b565681ee03a4fa814af0
nitsvutt <nitsvutt@gmail.com>
2023-06-29 09:35:39 +0700

    remove auto incremental column mysql

--- setup-source/mysql/mysql.sql
+++ setup-source/mysql/mysql.sql
@@ -1,5 +1,2 @@
 -- create database flight_delay in order to load data to
 create database if not exists flight_delay;
-
--- create an auto incremental id
-alter table flight_records add id int primary key auto_increment;

4ce3bbd2227139dc6f7d7ab6f8865daddc25634e
nitsvutt <nitsvutt@gmail.com>
2023-07-02 18:50:37 +0700

    load data to dim_date table

--- setup-warehouse/bigquery.sql
+++ setup-warehouse/bigquery.sql
@@ -3,7 +3,7 @@ create schema if not exists `flight_delay`;
 
 --create table dim_date
 create or replace table `flight_delay.dim_date`(
-  id INT64 not null,
+  id INT64,
   year INT64,
   quarter INT64,
   month INT64,
@@ -14,7 +14,7 @@ create or replace table `flight_delay.dim_date`(
 
 -- create table dim_airline
 create or replace table `flight_delay.dim_airline`(
-  id INT64 not null,
+  id INT64,
   reporting_airline STRING,
   dot_id_reporting_airline INT64,
   iata_code_reporting_airline STRING,
@@ -24,7 +24,7 @@ create or replace table `flight_delay.dim_airline`(
 
 -- create table dim_origin
 create or replace table `flight_delay.dim_origin`(
-  id INT64 not null,
+  id INT64,
   origin_airport_id INT64,
   origin_airport_seq_id INT64,
   origin_city_market_id INT64,
@@ -38,7 +38,7 @@ create or replace table `flight_delay.dim_origin`(
 
 -- create table dim_destination
 create or replace table `flight_delay.dim_destination`(
-  id INT64 not null,
+  id INT64,
   dest_airport_id INT64,
   dest_airport_seq_id INT64,
   dest_city_market_id INT64,
@@ -52,7 +52,7 @@ create or replace table `flight_delay.dim_destination`(
 
 -- create table dim_schedule
 create or replace table `flight_delay.dim_schedule`(
-  id INT64 not null,
+  id INT64,
   crs_dep_time INT64,
   dep_time_block STRING,
   crs_arr_time INT64,
@@ -64,7 +64,7 @@ create or replace table `flight_delay.dim_schedule`(
 
 -- create table dim_cancellations_and_diversions
 create or replace table `flight_delay.dim_cancellations_and_diversions`(
-  id INT64 not null,
+  id INT64,
   cancelled INT64,
   cancellation_code STRING,
   diverted INT64
@@ -72,7 +72,7 @@ create or replace table `flight_delay.dim_cancellations_and_diversions`(
 
 -- create table fact_records
 create or replace table `flight_delay.fact_records`(
-  id INT64 not null,
+  id INT64,
   date_id INT64,
   airline_id INT64,
   origin_id INT64,
@@ -83,15 +83,16 @@ create or replace table `flight_delay.fact_records`(
   dep_delay INT64,
   dep_delay_minutes INT64,
   dep_delay_15 INT64,
-  departure_delay_groups INT64,
+  dep_delay_groups INT64,
   taxi_out INT64,
   taxi_in INT64,
   wheels_off INT64,
   wheels_on INT64,
   arr_time INT64,
+  arr_delay INT64,
   arr_delay_minutes INT64,
   arr_delay_15 INT64,
-  arrival_delay_groups INT64,
+  arr_delay_groups INT64,
   actual_elapsed_time INT64,
   air_time INT64,
   flights INT64,

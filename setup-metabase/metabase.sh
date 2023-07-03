1afa657624c696a726ab5cfd5c5843b4af2a5fad
nitsvutt <nitsvutt@gmail.com>
2023-07-03 14:13:31 +0700

    set up metabase with postgressql database

--- setup-metabase/metabase.sh
+++ setup-metabase/metabase.sh
@@ -2,10 +2,18 @@
 cd ~/Desktop/metabase/
 wget https://downloads.metabase.com/v0.46.6/metabase.jar
 
+# set up posgresql to store metabase data
+brew install postgresql
+
+# create root user
+# psql command
+create role root with login password 'admin';
+create database metabase_app;
+grant all privileges on database metabase_app to root;
+
 # set up database
-createdb metabaseappdb
 export MB_DB_TYPE=postgres
-export MB_DB_DBNAME=metabaseappdb
+export MB_DB_DBNAME=metabase_data
 export MB_DB_PORT=5432
 export MB_DB_USER=root
 export MB_DB_PASS=admin

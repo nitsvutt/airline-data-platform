54255a45fa80ffd26af1d0e837188bb67cbfeb83
nitsvutt <nitsvutt@gmail.com>
2023-07-04 22:39:06 +0700

    set up metabase with postgresql database

--- setup-metabase/metabase.sh
+++ setup-metabase/metabase.sh
@@ -1,23 +1,26 @@
-# get jar file
-cd ~/Desktop/metabase/
-wget https://downloads.metabase.com/v0.46.6/metabase.jar
+#!bin/sh
 
-# set up posgresql to store metabase data
-brew install postgresql
-
-# create root user
-# psql command
+### set up database
+# create root user (psql command)
 create role root with login password 'admin';
-create database metabase_app;
-grant all privileges on database metabase_app to root;
+create database metabase_data;
+grant all on database metabase_data to root;
+\c metabase_data
+grant all on schema public to root;
 
-# set up database
+
+# export environment varriables
 export MB_DB_TYPE=postgres
 export MB_DB_DBNAME=metabase_data
 export MB_DB_PORT=5432
 export MB_DB_USER=root
 export MB_DB_PASS=admin
 export MB_DB_HOST=localhost
+
+### set up metabase
+# get jar file
+cd ~/Desktop/metabase/
+wget https://downloads.metabase.com/v0.46.6/metabase.jar
 
 # run this file
 java -jar metabase.jar
